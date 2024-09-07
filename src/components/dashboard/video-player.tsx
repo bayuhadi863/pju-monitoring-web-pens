981ba5791eb8900c02ce66f6e4cb51a0ac49ba74
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-09-07 16:24:24 +0700

    feat(chatbot): Adding chatbot page

--- src/components/dashboard/video-player.tsx
+++ src/components/dashboard/video-player.tsx
@@ -3,14 +3,19 @@ import React from 'react';
 const VideoPlayer: React.FC = () => {
   return (
     <div className='rounded-md'>
-      <iframe
+      {/* <iframe
         className='rounded-md w-full aspect-video'
         src='https://www.youtube.com/embed/jT8GMOupe3k?si=bJDlrIhQ6AUIquTY'
         title='YouTube video player'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
         referrerPolicy='strict-origin-when-cross-origin'
         allowFullScreen
-      ></iframe>
+      ></iframe> */}
+      <img
+        src='https://c993g0v4-3000.asse.devtunnels.ms/cctv/stream/1'
+        alt='live-cctv'
+        className='rounded-md w-full aspect-video'
+      />
     </div>
   );
 };
