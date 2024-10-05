b9db47bd8bf7aaacff9ecaba49b9eee5b5ef6661
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-10-05 23:40:02 +0700

    feat(dashboard_footer): make the footer center in mobile device

--- src/components/dashboard/dashboard-footer.tsx
+++ src/components/dashboard/dashboard-footer.tsx
@@ -11,7 +11,7 @@ const DashboardFooter: React.FC<DashboardFooterProps> = ({ className }) => {
   return (
     <footer className={`py-2 text-sm text-muted-foreground ${className ? className : ''}`}>
       <div className='container mx-auto px-4'>
-        <div className='flex flex-wrap justify-between items-center'>
+        <div className='flex flex-col md:flex-row justify-center md:justify-between items-center gap-1'>
           <p>Tim Pengabdian Masyarakat PENS</p>
           <div className='flex items-center'>
             <Copyright className='w-4 h-4 mr-1' />
