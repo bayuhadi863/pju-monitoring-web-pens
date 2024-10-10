7b9220949e651fa5fcc2c133fd112fcea27b4874
M. Ainur Ramadhan <134328737+Ramadhan1710@users.noreply.github.com>
2024-10-10 22:18:27 +0700

    feat/profile : add ui account page

---
+++ src/components/account/delete-card.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Card, CardHeader, CardTitle, CardDescription, CardFooter } from "@/components/ui/card";
+
+const DeleteCard: React.FC = () => {
+  const handleDelete = () => {
+    // Logic for delete action
+    console.log("Deleting user...");
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Delete User</CardTitle>
+        <CardDescription>Are you sure you want to delete this user?</CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <div className="flex justify-start w-full">
+          <button
+            type="button"
+            onClick={handleDelete}
+            className="w-full bg-red-500 text-white py-2 px-4 rounded-lg"
+          >
+            Delete
+          </button>
+        </div>
+
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default DeleteCard;
--- src/pages/dashboard/account-page.tsx
+++ src/pages/dashboard/account-page.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+import UpdateCard from "@/components/account/update-card";
+import DeleteCard from "@/components/account/delete-card";
 
 const AccountPage: React.FC = () => {
-  return <div>AccountPage</div>;
+  return (
+    <div className="space-y-6 mt-4">
+      <UpdateCard />
+      <DeleteCard />
+    </div>
+  );
 };
 
 export default AccountPage;
