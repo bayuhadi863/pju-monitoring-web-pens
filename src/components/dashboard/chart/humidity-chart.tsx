ccad975f52fb028c1bf485db9152c801e0e36f56
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-10-26 22:52:10 +0700

    feat(humidity): improve humidity chart

--- src/components/dashboard/chart/humidity-chart.tsx
+++ src/components/dashboard/chart/humidity-chart.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { CartesianGrid, Line, LineChart, XAxis } from 'recharts';
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts';
 import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
 import { SensorChartResponse } from '@/lib/types/response/chart/sensor-chart-response';
 import { getSensorChartData } from '@/lib/services/chart-service';
@@ -51,9 +51,29 @@ const HumidityChart: React.FC = () => {
           tickMargin={8}
           tickFormatter={(hour) => `${hour}:00`}
         />
+        <YAxis
+          tickLine={false}
+          axisLine={false}
+          tickMargin={8}
+          tickFormatter={(value) => `${value}%`}
+          dataKey='averageValue'
+        />
         <ChartTooltip
           cursor={true}
-          content={<ChartTooltipContent />}
+          content={
+            <ChartTooltipContent
+              hideLabel
+              formatter={(value, name) => (
+                <div className='flex min-w-[130px] items-center text-xs font-medium gap-2'>
+                  {chartConfig[name as keyof typeof chartConfig]?.label || name}
+                  <div className='ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums text-foreground'>
+                    {value}
+                    <span className='font-medium text-muted-foreground'>%</span>
+                  </div>
+                </div>
+              )}
+            />
+          }
         />
         <Line
           dataKey='averageValue'
@@ -71,8 +91,8 @@ const HumidityChart: React.FC = () => {
     </ChartContainer>
   );
 
-  const loadingComponent = <div className='h-80 flex justify-center items-center'>Loading...</div>;
-  const errorComponent = <div className='h-80 flex justify-center items-center'>{error}</div>;
+  const loadingComponent = <div className='h-72 flex justify-center items-center'>Loading...</div>;
+  const errorComponent = <div className='h-72 flex justify-center items-center'>{error}</div>;
 
   return <div>{isLoading ? loadingComponent : error ? errorComponent : chart}</div>;
 };
