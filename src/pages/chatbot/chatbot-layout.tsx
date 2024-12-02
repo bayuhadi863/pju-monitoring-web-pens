27e50c24377950abe3f9ad6f29b4e65874eab0cb
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-12-02 21:44:32 +0700

    fix(chatbot): fix delete conversation error

--- src/pages/chatbot/chatbot-layout.tsx
+++ src/pages/chatbot/chatbot-layout.tsx
@@ -13,114 +13,114 @@ import { toast } from '@/components/hooks/use-toast';
 import { getUserId } from '@/lib/utils/storage';
 
 const SidebarHeader = () => {
-  return <h1 className='text-xl font-semibold'>Chatbot</h1>;
+    return <h1 className='text-xl font-semibold'>Chatbot</h1>;
 };
 
 const ChatbotLayout: React.FC = () => {
-  const apiBaseUrl = import.meta.env.VITE_CHATBOT_API_BASE_URL || 'http://localhost:5000';
-  console.log(apiBaseUrl);
-  const { conversationId } = useParams();
-  const navigate = useNavigate();
-  const userId = getUserId();
-  const [triggerFetch, setTriggerFetch] = useState(false);
-  const [sidebarLinks, setSidebarLinks] = useState<SidebarLinkDataType[]>([
-    {
-      to: '/chatbot',
-      label: 'Buat obrolan baru',
-    },
-  ]);
-
-  const handleDeleteConversation = (conversationIdDelete: string) => {
-    axios
-      .delete(`https://bambubot.up.railway.app/conversations/${conversationIdDelete}`, {
-        headers: {
-          Accept: 'application/json',
-          Authorization: userId,
+    const apiBaseUrl = import.meta.env.VITE_CHATBOT_API_BASE_URL || 'http://localhost:5000';
+    console.log(apiBaseUrl);
+    const { conversationId } = useParams();
+    const navigate = useNavigate();
+    const userId = getUserId();
+    const [triggerFetch, setTriggerFetch] = useState(false);
+    const [sidebarLinks, setSidebarLinks] = useState<SidebarLinkDataType[]>([
+        {
+            to: '/chatbot',
+            label: 'Buat obrolan baru',
         },
-      })
-      .then(() => {
-        setTriggerFetch((prev) => !prev);
-        toast({
-          variant: 'success',
-          duration: 3000,
-          title: 'Chat telah dihapus!',
-          description: 'Anda berhasil menghapus chat ini.',
-        });
-        if (conversationId === conversationIdDelete) {
-          navigate('/chatbot');
-        }
-      })
-      .catch((error) => {
-        console.error('Error deleting conversation:', error);
-        toast({
-          variant: 'destructive',
-          duration: 3000,
-          title: 'Gagal menghapus chat!',
-          description: 'Silahkan coba lagi nanti.',
-        });
-      });
-  };
+    ]);
 
-  useEffect(() => {
-    axios
-      .get(`https://bambubot.up.railway.app/api/conversations`, {
-        headers: {
-          Accept: 'application/json',
-          Authorization: userId,
-        },
-      })
-      .then((response) => {
-        const data = response.data;
-        const newLinks = data.map((conversation: { id: string; title: string }) => ({
-          to: `/chatbot/${conversation.id}`,
-          label: conversation.title,
-          removeIcon: <GoTrash />,
-          onRemove: () => handleDeleteConversation(conversation.id),
-        }));
+    const handleDeleteConversation = (conversationIdDelete: string) => {
+        axios
+            .delete(`https://bambubot.up.railway.app/api/conversations/${conversationIdDelete}`, {
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: userId,
+                },
+            })
+            .then(() => {
+                setTriggerFetch((prev) => !prev);
+                toast({
+                    variant: 'success',
+                    duration: 3000,
+                    title: 'Chat telah dihapus!',
+                    description: 'Anda berhasil menghapus chat ini.',
+                });
+                if (conversationId === conversationIdDelete) {
+                    navigate('/chatbot');
+                }
+            })
+            .catch((error) => {
+                console.error('Error deleting conversation:', error);
+                toast({
+                    variant: 'destructive',
+                    duration: 3000,
+                    title: 'Gagal menghapus chat!',
+                    description: 'Silahkan coba lagi nanti.',
+                });
+            });
+    };
 
-        setSidebarLinks((prevLinks) => {
-          const firstLink = prevLinks[0];
-          const filteredLinks = prevLinks.slice(1).filter((link) => data.some((conversation: { id: any }) => `/chatbot/${conversation.id}` === link.to));
-          const uniqueNewLinks = newLinks.filter((newLink: { to: string }) => !filteredLinks.some((prevLink) => prevLink.to === newLink.to));
-          return [firstLink, ...filteredLinks, ...uniqueNewLinks];
-        });
-      })
-      .catch((error) => {
-        console.error('Error fetching conversations:', error);
-      });
-  }, [triggerFetch]);
+    useEffect(() => {
+        axios
+            .get(`https://bambubot.up.railway.app/api/conversations`, {
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: userId,
+                },
+            })
+            .then((response) => {
+                const data = response.data;
+                const newLinks = data.map((conversation: { id: string; title: string }) => ({
+                    to: `/chatbot/${conversation.id}`,
+                    label: conversation.title,
+                    removeIcon: <GoTrash />,
+                    onRemove: () => handleDeleteConversation(conversation.id),
+                }));
 
-  return (
-    <div>
-      <Sidebar
-        headerContent={<SidebarHeader />}
-        content={
-          <SidebarLinks
-            links={sidebarLinks}
-            fontWeight='font-normal'
-          />
-        }
-      />
-      <div className='lg:ml-64 bg-muted dark:bg-slate-900 h-screen flex flex-col relative'>
-        <div className='flex items-center gap-3 absolute inset-x-0 px-4 bg-muted dark:bg-slate-900 border-b-2 border-dashed'>
-          <MobileSidebar
-            headerContent={<SidebarHeader />}
-            content={
-              <MobileSidebarLinks
-                links={sidebarLinks}
-                fontWeight='font-normal'
-              />
-            }
-          />
-          <ChatbotHeader />
+                setSidebarLinks((prevLinks) => {
+                    const firstLink = prevLinks[0];
+                    const filteredLinks = prevLinks.slice(1).filter((link) => data.some((conversation: { id: any }) => `/chatbot/${conversation.id}` === link.to));
+                    const uniqueNewLinks = newLinks.filter((newLink: { to: string }) => !filteredLinks.some((prevLink) => prevLink.to === newLink.to));
+                    return [firstLink, ...filteredLinks, ...uniqueNewLinks];
+                });
+            })
+            .catch((error) => {
+                console.error('Error fetching conversations:', error);
+            });
+    }, [triggerFetch]);
+
+    return (
+        <div>
+            <Sidebar
+                headerContent={<SidebarHeader />}
+                content={
+                    <SidebarLinks
+                        links={sidebarLinks}
+                        fontWeight='font-normal'
+                    />
+                }
+            />
+            <div className='lg:ml-64 bg-muted dark:bg-slate-900 h-screen flex flex-col relative'>
+                <div className='flex items-center gap-3 absolute inset-x-0 px-4 bg-muted dark:bg-slate-900 border-b-2 border-dashed'>
+                    <MobileSidebar
+                        headerContent={<SidebarHeader />}
+                        content={
+                            <MobileSidebarLinks
+                                links={sidebarLinks}
+                                fontWeight='font-normal'
+                            />
+                        }
+                    />
+                    <ChatbotHeader />
+                </div>
+                <main className='h-full'>
+                    {/* <Outlet /> */}
+                    <Outlet context={{ setTriggerFetch }} />
+                </main>
+            </div>
         </div>
-        <main className='h-full'>
-          {/* <Outlet /> */}
-          <Outlet context={{ setTriggerFetch }} />
-        </main>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ChatbotLayout;
