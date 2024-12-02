211e3b79524cadc651b27237a8ebb142cd5b1cfc
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-12-02 21:30:45 +0700

    feat(chatbot): implement chatbot for non auth user

--- src/lib/utils/storage.ts
+++ src/lib/utils/storage.ts
@@ -1,14 +1,23 @@
 import CryptoJS from 'crypto-js';
+import { v4 as uuidv4 } from 'uuid';
 // User ID
 const secretKey = 'Its not a bug, its an undocumented feature';
-export const getUserId = (): string | null => {
-    const encryptedId = localStorage.getItem('userId');
+export const getUserId = (): string => {
+    let encryptedId = localStorage.getItem('userId');
+
+    // Jika tidak ada encryptedId, generate UUID baru dan set di localStorage
     if (!encryptedId) {
-        return null;
+        const userId = uuidv4(); // Generate UUID v4
+        encryptedId = CryptoJS.AES.encrypt(userId, secretKey).toString();
+        localStorage.setItem('userId', encryptedId);
+        return userId; // Kembalikan UUID yang baru dibuat
     }
+
+    // Jika ada encryptedId, decrypt dan kembalikan userId
     const bytes = CryptoJS.AES.decrypt(encryptedId, secretKey);
     return bytes.toString(CryptoJS.enc.Utf8);
 };
+
 export const setUserId = (userId: string): void => {
     const encryptedId = CryptoJS.AES.encrypt(userId.toString(), secretKey).toString();
     localStorage.setItem('userId', encryptedId);
