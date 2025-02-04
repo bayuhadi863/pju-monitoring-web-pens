12f09e45335ae997031cdfd4aef19b0746f0cf7f
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2025-02-04 19:45:12 +0700

    feat(cctv): add video player

--- src/components/dashboard/video/video-player2.tsx
+++ src/components/dashboard/video/video-player2.tsx
@@ -1,28 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Hls from 'hls.js';
 
 const VideoPlayer2: React.FC = () => {
-  const hostName = import.meta.env.VITE_VPS_HOST;
-  useEffect(() => {
-    const video = document.getElementById('cctv-video') as HTMLVideoElement | null;
+  const baseUrl = import.meta.env.VITE_VPS_BASE_URL;
+  // useEffect(() => {
+  //   const video = document.getElementById(
+  //     'cctv-video'
+  //   ) as HTMLVideoElement | null;
 
-    if (video) {
+  //   if (video) {
+  //     if (Hls.isSupported()) {
+  //       const hls = new Hls();
+  //       hls.loadSource(`${baseUrl}/live/simpanglima.m3u8`);
+  //       hls.attachMedia(video);
+  //     } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+  //       video.src = `${baseUrl}/live/simpanglima.m3u8`;
+  //     }
+  //   } else {
+  //     console.error('Video element not found');
+  //   }
+  // }, []);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      const video = videoRef.current;
+      const videoSrc = `${baseUrl}/live/simpanglima.m3u8`;
+
       if (Hls.isSupported()) {
         const hls = new Hls();
-        hls.loadSource(`http://${hostName}/live/simpanglima.m3u8`);
+        hls.loadSource(videoSrc);
         hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          video.play().catch((err) => console.log('Playback failed:', err));
+        });
       } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        video.src = `http://${hostName}/live/simpanglima.m3u8`;
+        video.src = videoSrc;
+        video.addEventListener('loadedmetadata', () => {
+          video.play().catch((err) => console.log('Playback failed:', err));
+        });
       }
-    } else {
-      console.error('Video element not found');
     }
   }, []);
 
   return (
     <div className="rounded-md aspect-video">
       <video
-        id="cctv-video"
+        ref={videoRef}
         controls
         autoPlay
         className="rounded-md w-full aspect-video"
